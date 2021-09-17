f6ef3bd62c2d9fe9fa488355e0ed7075f13286c1
Rahman Elsirafy <rahman2408@gmail.com>
2021-09-17 04:57:33 -0400

    commit tree bbeb277d5867d0554211f29756f38b5f737025cf

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(dArry, driver) {
+  return  dArry.filter(d => {
+    return d.toLowerCase() === driver.toLowerCase();
+  })
+}
+
+function fuzzyMatch(dArry, letters) {
+  return dArry.filter(d => {
+    return d[0] === letters[0] || d[0,1] == letters[0,1]
+  })
+}
+
+function matchName(dArry, name) {
+  return dArry.filter(d => {
+    return d.name === name
+  })
+}
